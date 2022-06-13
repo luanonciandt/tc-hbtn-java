27a5a314fb1537f712518e46f9a73896db94e73b
luanonciandt <luansena@ciandt.com>
2022-06-13 13:56:12 -0300

    sexta task corrigida

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -2,6 +2,6 @@ public class PrintFormatacaoNumeros {
     public static void main(String[] args) {
         float taxa = 0.2456f;
         float valor = 7654.321f;
-        System.out.printf("Valor: R$ %2f\nTaxa: %.2f%%\n", valor, taxa);
+        System.out.printf("Valor: R$ %.2f\nTaxa: %.2f%%\n", valor, taxa);
     }
 }
