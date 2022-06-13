1453422340ab3ab458dda837040f88617896e061
luanonciandt <luansena@ciandt.com>
2022-06-13 14:11:47 -0300

    sexta task corrigida #5

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -1,8 +1,7 @@
-import java.text.NumberFormat;
 public class PrintFormatacaoNumeros {
     public static void main(String[] args) {
         float taxa = 0.2456f;
         float valor = 7654.321f;
-        System.out.printf("Valor: R%s\nTaxa: %.2f%%\n", NumberFormat.getCurrencyInstance().format(valor), taxa);
+        System.out.printf("Valor: R$ %,.2f\nTaxa: %.2f%%\n", valor, taxa);
     }
 }
