44209585d6f26b25ad19b287c4c2fe72677846c9
luanonciandt <luansena@ciandt.com>
2022-06-15 10:48:07 -0300

    encapsulamento correcao #1

---
+++ 0x04/encapsulamento/PersonagemJava.java
@@ -0,0 +1,20 @@
+public class PersonagemJava {
+    private int saudeAtual;
+    private String nome;
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
