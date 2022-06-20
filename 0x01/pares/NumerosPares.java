bd4eeed3302dcb424ecafb848177e0c321438ac4
luanonciandt <luansena@ciandt.com>
2022-06-20 16:08:05 -0300

    numeros pares de 0 a 98

---
+++ 0x01/pares/NumerosPares.java
@@ -0,0 +1,12 @@
+public class NumerosPares {
+    public static void main(String[] args) {
+        for(int i = 0; i < 100; i+=2) {
+           if (i == 98) {
+               System.out.println(i);
+           } else {
+               System.out.print(i + ", ");
+           }
+        }
+    }
+
+}
