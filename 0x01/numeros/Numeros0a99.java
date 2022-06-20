486e83c3f7cc5f0448c39e2af5169387189ad46c
luanonciandt <luansena@ciandt.com>
2022-06-20 16:07:03 -0300

    numeros de 0 a 99

---
+++ 0x01/numeros/Numeros0a99.java
@@ -0,0 +1,12 @@
+public class Numeros0a99 {
+
+    public static void main(String[] args) {
+        for(int i = 0; i < 100; i++) {
+            if (i == 99) {
+                System.out.println(i);
+            } else {
+                System.out.print(i + ", ");
+            }
+        }
+    }
+}
