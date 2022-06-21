e8f50f4926cf3623ba81d37caf976a0e0bf0ecb1
luanonciandt <luansena@ciandt.com>
2022-06-21 13:29:20 -0300

    task 1 excecoes

---
+++ 0x02/quadrado/Quadrado.java
@@ -0,0 +1,7 @@
+public class Quadrado {
+
+    public static double area(double lado) {
+        if (lado < 1) throw new IllegalArgumentException("Lado deve possuir valor positivo");
+        return lado * lado;
+    }
+}
